4b0e590250641535b70f9a312739e3eca70d25f8
gennyble <gen@nyble.dev>
2020-02-05 22:36:18 -0600

    Add fn prototypes to document with pseudocode

--- src/document.rs
+++ src/document.rs
@@ -11,7 +11,16 @@ impl Document {
         }
     }
 
-    pub fn process_includes(&mut self) {
+   /*pub fn set_variable(&mut self, key: &str, value: &str) -> usize {
+        /*
+        Replace all instances of Element::Variable(key) with
+            Element::Text(value).
+        Make sure to NOT replace anything in a template
+        return number of times it was replaced
+        */
+    }
+
+    /*pub fn process_includes(&mut self) -> Result<Error, usize>{
         /*
         @START
         Use binary_search to find an include, any include.
@@ -23,4 +32,21 @@ impl Document {
         @goto start while no more includes
         */
     }
+
+    pub fn get_template(&self, name: &str) -> Result<Error, Document> {
+        /*
+        Find the TemplateStart with the name `name`
+        Work until the first TemplateEnd, tempaltes can't overlap
+        Return these elements as a new document
+        */
+    }
+
+    pub fn as_string() -> String {
+        /*
+        Maybe this would be better as like IntoString
+        make sure to remove anything that isn't Text
+        also remove everything in between and including templates
+        cat all the Element::Text-s together
+        */
+    }*/
 }
